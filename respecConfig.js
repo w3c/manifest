176a211e29bcc1a1dff67db52c39f94fc76ac017
Marcos Caceres <marcos@marcosc.com>
2013-11-26 17:35:01 +0000

    added external respec config

---
+++ respecConfig.js
@@ -0,0 +1,47 @@
+var respecConfig = {
+    specStatus: 'FPWD',
+    shortName: 'manifest',
+    subtitle: 'Living Standard',
+    edDraftURI: 'http://www.w3.org/2008/webapps/manifest/',
+    editors: [{
+        name: 'Marcos Caceres',
+        company: 'Mozilla Corporation',
+        companyURL: 'http://mozilla.com'
+    }, {
+        name: 'Anssi Kostiainen',
+        company: 'Intel Corporation',
+        companyURL: 'http://intel.com'
+    }, {
+        name: 'Kenneth Rohde Christiansen',
+        company: 'Intel Corporation',
+        companyURL: 'http://intel.com'
+    }],
+    wg: 'Web Applications (WebApps) Working Group',
+    wgURI: 'http://www.w3.org/2008/webapps/',
+    wgPublicList: 'public-webapps',
+    wgPatentURI: 'http://www.w3.org/2004/01/pp-impl/42538/status',
+    otherLinks: [{
+        key: 'Repository',
+        data: [{
+            value: 'We are on Github.',
+            href: 'https://github.com/sysapps/manifest'
+        }, {
+            value: 'File a bug.',
+            href: 'https://github.com/sysapps/manifest/issues'
+        }, {
+            value: 'Commit history.',
+            href: 'https://github.com/sysapps/manifest/commits/gh-pages'
+        }]
+    }],
+    localBiblio: {
+        'FETCH': {
+            'authors': [
+                'Anne van Kesteren'
+            ],
+            'href': 'http://fetch.spec.whatwg.org/',
+            'title': 'Fetch',
+            'status': 'Living Standard',
+            'publisher': 'WHATWG'
+        }
+    }
+};
