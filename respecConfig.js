445e1ae20ffc981f380ecfab07a8cb9e1014678d
Marcos Caceres <marcosscaceres@gmail.com>
2013-12-02 20:29:17 +1000

    W3C - First public working draft
    Prepared document as FPWD
    
    Fixed ed draft link (closes #71)

--- respecConfig.js
+++ respecConfig.js
@@ -1,8 +1,10 @@
 var respecConfig = {
-    specStatus: 'FPWD',
-    shortName: 'manifest',
-    subtitle: 'Living Standard',
-    edDraftURI: 'http://www.w3.org/2008/webapps/manifest/',
+    specStatus: 'ED',
+    shortName: 'appmanifest',
+    subtitle: 'Living Document',
+    edDraftURI: 'http://w3c.github.io/manifest/',
+    previousMaturity: 'FPWD',
+    previousURI: 'http://www.w3.org/TR/2013/WD-appmanifest-20131217/',
     editors: [{
         name: 'Marcos Caceres',
         company: 'Mozilla Corporation',
@@ -21,6 +23,10 @@ var respecConfig = {
     wgPublicList: 'public-webapps',
     wgPatentURI: 'http://www.w3.org/2004/01/pp-impl/42538/status',
     otherLinks: [{
+            key: 'Latest Web Apps Version (for W3C process)',
+            data: [{href: 'http://w3c.github.io/manifest/releases/FPWD.html'}]
+        },
+        {
         key: 'Repository',
         data: [{
             value: 'We are on Github.',
